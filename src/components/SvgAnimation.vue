052fb2fec4be76da6e99dfbc6469bfdf6a7dfdb1
Damian Idczak <di.idczak@gmail.com>
2021-12-22 19:58:41 +0100

    div css animation

--- src/components/SvgAnimation.vue
+++ src/components/SvgAnimation.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="['shape-blob', `shape-blob--${index + 1}`]"
-    v-for="index in 4"
+    v-for="index in 8"
   ></div>
 </template>
 
@@ -18,11 +18,14 @@ export default {
   width: 200px;
   border-radius: 30% 50% 20% 40%;
   animation: transform 20s ease-in-out infinite both alternate,
-    movement_one 40s ease-in-out infinite both;
+    mv_one 30s ease-in-out infinite both;
   opacity: 0.1;
   position: absolute;
   left: 70%;
   top: 50%;
+  overflow: hidden;
+  z-index: -1;
+
   &--1 {
     height: 100px;
     width: 100px;
@@ -30,34 +33,63 @@ export default {
     top: 10%;
     transform: rotate(-180deg);
     animation: transform 30s ease-in-out infinite both alternate,
-      movement_two 60s ease-in-out infinite both;
+      mv_two 50s ease-in-out infinite both;
   }
   &--2 {
-    height: 350px;
+    height: 310px;
     width: 350px;
     left: 50%;
     top: 1%;
     transform: rotate(-180deg);
     animation: transform 30s ease-in-out infinite both alternate,
-      movement_two 60s ease-in-out infinite both;
+      mv_one 30s ease-in-out infinite both;
   }
   &--3 {
-    height: 100px;
-    width: 100px;
+    height: 200px;
+    width: 180px;
     left: 20%;
     top: 50%;
     transform: rotate(-180deg);
     animation: transform 30s ease-in-out infinite both alternate,
-      movement_two 60s ease-in-out infinite both;
+      mv_three 20s ease-in-out infinite both;
   }
   &--4 {
-    height: 100px;
-    width: 100px;
+    height: 70px;
+    width: 70px;
     left: 49%;
     top: 80%;
     transform: rotate(-180deg);
     animation: transform 30s ease-in-out infinite both alternate,
-      movement_two 30s ease-in-out infinite both;
+      mv_two 30s ease-in-out infinite both;
+  }
+  &--5 {
+    height: 440px;
+    width: 450px;
+    left: -20%;
+    top: 10%;
+    transform: rotate(-180deg);
+    animation: transform 30s ease-in-out infinite both alternate,
+      mv_three 40s ease-in-out infinite both;
+  }
+  &--6 {
+    height: 200px;
+    width: 181px;
+    left: 21%;
+    top: 50%;
+    transform: rotate(-10deg);
+    animation: transform 30s ease-in-out infinite both alternate,
+      mv_one 40s ease-in-out infinite both;
+    opacity: 0.03;
+  }
+
+  &--7 {
+    height: 140px;
+    width: 140px;
+    left: 50%;
+    top: 50%;
+    transform: rotate(-180deg);
+    animation: transform 30s ease-in-out infinite both alternate,
+      mv_two 30s ease-in-out infinite both;
   }
 }
 
@@ -80,23 +112,45 @@ export default {
   }
 }
 
-@keyframes movement_one {
+@keyframes mv_one {
   0%,
   100% {
     transform: none;
   }
   50% {
-    transform: translate(50%, 20%) rotateY(10deg) scale(1.2);
+    transform: translate(
+        percentage(random(100) / 100),
+        percentage(random(100) / 100)
+      )
+      rotateY(percentage(random(100) / 100) deg) scale(1.2);
   }
 }
 
-@keyframes movement_two {
+@keyframes mv_two {
   0%,
   500% {
     transform: none;
   }
   50% {
-    transform: translate(50%, 20%) rotate(-200deg) scale(1.2);
+    transform: translate(
+        percentage(random(100) / 100),
+        percentage(random(100) / 100)
+      )
+      rotate(percentage(random(100) / 100) deg) scale(1.2);
+  }
+}
+
+@keyframes mv_three {
+  0%,
+  200% {
+    transform: none;
+  }
+  40% {
+    transform: translate(
+        percentage(random(100) / 100),
+        percentage(random(100) / 100)
+      )
+      rotateY(percentage(random(100) / 100) deg) scale(1.2);
   }
 }
 </style>
